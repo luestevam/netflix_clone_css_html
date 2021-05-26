fd17402dc0901ab6128103305198680157a472bd
luestevam@hotmail.com <luestevam@hotmail.com>
2021-05-26 19:16:05 -0300

    Olx Clone

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:olx/home.dart';
+
+void main () => runApp(MaterialApp(
+  title: "OLX",
+  home: Home(),
+  debugShowCheckedModeBanner: false,
+
+));
