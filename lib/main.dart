14c1ece8e392c69fb9e4b14d38733f8b1eda6cca
luestevam@hotmail.com <luestevam@hotmail.com>
2021-05-27 20:34:12 -0300

    MenuBotton

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:olx/home.dart';
+import 'package:olx/route_generator.dart';
+import 'package:olx/views/anuncios.dart';
+
+
+final ThemeData TemaPadrao = ThemeData(
+  primaryColor: Color(0xff9c27b0),
+  accentColor: Color(0xff7b1f02),
+
+);
 
 void main () => runApp(MaterialApp(
   title: "OLX",
-  home: Home(),
+  home: Anuncios(),
+  theme: TemaPadrao,
+  initialRoute: "/",
+  onGenerateRoute: RouteGenerator.generateRoute,
   debugShowCheckedModeBanner: false,
 
 ));
---
+++ lib/views/anuncios.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Anuncios extends StatefulWidget {
+  @override
+  _AnunciosState createState() => _AnunciosState();
+}
+
+class _AnunciosState extends State<Anuncios> {
+
+  List<String> itensMenu = [];
+
+  _escolherMenuItem(String itemEscolhido) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("OLX"),
+        elevation: 0,
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: _escolherMenuItem,
+            itemBuilder: (contex) {
+              return itensMenu.map((String item) {
+                return PopupMenuItem<String>(
+                  value: item,
+                  child: Text(item),
+                );
+              }).toList();
+            },
+          )
+        ],
+      ),
+      body: Container(
+        child: Text("Anúncios"),
+      ),
+    );
+  }
+}
